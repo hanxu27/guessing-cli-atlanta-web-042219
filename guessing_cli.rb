bab078fccb048bd0220be2e07e6517278910ec31
han xu <hanxu27@gmail.com>
2019-04-16 16:16:37 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,6 +6,8 @@ def run_guessing_game
   while guess != "exit" do
     puts "Guess a number between 1 and 10"
     guess = gets.chomp!
-    
+    if guess == number
+      puts "You guess the correct number!"
+    end
   end
 end
