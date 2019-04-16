db25ca08009b750d6eb19c57a359b21c76361381
han xu <hanxu27@gmail.com>
2019-04-16 16:40:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -12,7 +12,7 @@ def run_guessing_game
     elsif guess.to_i == number
       puts "You guessed the correct number!"
     elsif guess.to_i != number
-      puts "The computer guessed #{number}"
+      puts "The computer guessed #{number}."
     end
   end
 end
