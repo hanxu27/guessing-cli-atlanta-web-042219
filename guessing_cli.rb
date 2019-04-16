0d4d92025d26db63c824fc2efd818a3f83fc277b
han xu <hanxu27@gmail.com>
2019-04-16 16:22:24 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   number = rand(1..6)
   
   while guess != "exit" do
-    puts "Guess a number between 1 and 6"
+    puts "Guess a number between 1 and 6."
     guess = gets.chomp
     if guess == number
       puts "You guess the correct number!"
